beb463e4a55f2dc054c43e182a9ca4f2156242af
mxayon <mxnkpl@gmail.com>
2022-09-12 16:33:07 -0700

    tries to set up counter for next button click

--- carousel/script.js
+++ carousel/script.js
@@ -4,3 +4,16 @@ pages.forEach((page, i) => {
     page.style.transform = `translateX(${i * 100}%)`;
 });
 
+let pageCount = 0;
+
+const nextPage = document.querySelector(".btn-next");
+
+nextPage.addEventListener("click", function () {
+    pageCount++;
+
+    pages.forEach((page, i) => {
+        page.style.transform = `translateX(${100 * (i - pageCount)}%)`;
+    });
+    console.log(pageCount);
+    
+});
