84ddf1a2f376ad9d40064d1dbee355f0dafcfe46
mxayon <mxnkpl@gmail.com>
2022-09-13 15:40:46 -0700

    code

--- carousel/script.js
+++ carousel/script.js
@@ -1,19 +1,20 @@
 const pages = document.querySelectorAll(".images");
 
 console.log(document.getElementsByClassName('button'));
-console.log(document.querySelector('body button'))
+console.log(document.getElementsByTagName("button"));
 
 pages.forEach((page, i) => {
     page.style.transform = `translateX(${i * 100}%)`;
 });
 
 let pageCount = 0;
-const nextPage = document.querySelector("button[title]");
+
+const nextPage = document.querySelector("button");
 
 
-nextPage.addEventListener("click", function onClick() {
+nextPage.addEventListener("click", function () {
     pageCount++;
-    console.log('pageCount');
+    console.log(pageCount);
 
     pages.forEach((page, i) => {
         page.style.transform = `translateX(${100 * (i - pageCount)}%)`;
