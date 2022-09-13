c2d258605ae1e74b2ed751dcaa57480b5c83bce8
mxayon <mxnkpl@gmail.com>
2022-09-13 14:08:00 -0700

    using querySelector to grab button

--- carousel/script.js
+++ carousel/script.js
@@ -1,19 +1,21 @@
 const pages = document.querySelectorAll(".images");
 
+console.log(document.getElementsByClassName('button'));
+console.log(document.querySelector('body button'))
+
 pages.forEach((page, i) => {
     page.style.transform = `translateX(${i * 100}%)`;
 });
 
 let pageCount = 0;
+const nextPage = document.querySelector("button[title]");
 
-const nextPage = document.querySelector(".btn-next");
 
-nextPage.addEventListener("click", function () {
+nextPage.addEventListener("click", function onClick() {
     pageCount++;
+    console.log('pageCount');
 
     pages.forEach((page, i) => {
         page.style.transform = `translateX(${100 * (i - pageCount)}%)`;
     });
-    console.log(pageCount);
-    
 });
