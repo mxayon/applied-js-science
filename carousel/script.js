34f0dc65aa1948a657f6d3ab351fc6a9a36ddd50
mxayon <mxnkpl@gmail.com>
2022-09-14 17:05:23 -0700

    switching gears

--- carousel/script.js
+++ carousel/script.js
@@ -1,22 +1,25 @@
 const pages = document.querySelectorAll(".images");
 
-console.log(document.getElementsByClassName('button'));
+console.log(document.getElementsByClassName('btn-next'));
 console.log(document.getElementsByTagName("button"));
+console.log(document.querySelector([title="b"]));
+console.log(document.querySelector('button.btn.btn-next'));
+console.log(getElementsByClassName('btn-next'));
 
 pages.forEach((page, i) => {
     page.style.transform = `translateX(${i * 100}%)`;
 });
 
+const nextPage = document.getElementsByClassName('btn-next');
+
 let pageCount = 0;
+let maxPage = pages.length - 1;
 
-const nextPage = document.querySelector("button");
-
-
-nextPage.addEventListener("click", function () {
-    pageCount++;
-    console.log(pageCount);
-
-    pages.forEach((page, i) => {
-        page.style.transform = `translateX(${100 * (i - pageCount)}%)`;
-    });
+nextPage.addEventListener("click", () => {
+    if (pageCount === maxPage) {
+        pageCount = 0;
+    } else {
+        pageCount++;
+        console.log(pageCount);
+    }
 });
