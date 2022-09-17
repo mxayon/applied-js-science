54690ef089f7ee29adc0a47bf4ad39b56282b91e
Maximo P. Nakpil <maxpowers@Maximos-MacBook-Pro.local>
2022-09-16 19:35:16 -0700

    turbo log

--- carousel-2/script.js
+++ carousel-2/script.js
@@ -1,12 +1,16 @@
 class Slider {
-    constructor(slidePiece) {
-        this.slider = slidePiece;
-        this.slideI = sliderPiece.getElementsByClassName("slide");
-        this.nextB = slidePiece.querySelector("next");
-        this.prevB = slidePiece.querySelector("prev");
-        this.i = 0;
-        this.prevSlideIndx = this.slideI.length - 1;
-        this.nextSlideIndx = 1;
-        this.isSliding = false;
-    }
+  constructor(slidePiece) {
+    console.log(
+      "🚀 ~ file: script.js ~ line 3 ~ Slider ~ constructor ~ slidePiece",
+      slidePiece
+    );
+    this.slider = slidePiece;
+    this.slideI = slidePiece.getElementsByClassName("slide");
+    this.nextB = slidePiece.querySelector("next");
+    this.prevB = slidePiece.querySelector("prev");
+    this.i = 0;
+    this.prevSlideIndx = this.slideI.length - 1;
+    this.nextSlideIndx = 1;
+    this.isSliding = false;
+  }
 }
